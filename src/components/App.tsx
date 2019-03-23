24483bdd9b0c0bf4964ab490a5b838f5d2d9d527
Basti Buck <mail@bastibuck.de>
2019-03-23 19:33:17 +0100

    project setup

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "../../node_modules/tent-css/dist/tent.min.css";
+
+class App extends Component {
+  public render() {
+    return (
+      <div>
+        <h1>asd</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
