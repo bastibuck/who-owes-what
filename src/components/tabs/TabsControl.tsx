cadf6c2774087b9846e8a25afea064fdabfedfa7
Basti Buck <mail@bastibuck.de>
2019-03-23 21:41:09 +0100

    add state to tabs and use FontAwesome React

--- src/components/tabs/TabsControl.tsx
+++ src/components/tabs/TabsControl.tsx
@@ -1,32 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChartBar,
+  faDollarSign,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
+enum ETabs {
+  FRIENDS,
+  EXPENSES,
+  RESULT,
+}
+
+const IconContainer = styled.span.attrs({ className: "icon is-small" })``;
 
 const TabsControl = () => {
+  const [activeTab, setActiveTab] = useState(ETabs.FRIENDS);
+
+  const handleClickFriends = (e: React.MouseEvent) => {
+    setActiveTab(ETabs.FRIENDS);
+  };
+
+  const handleClickExpenses = (e: React.MouseEvent) => {
+    setActiveTab(ETabs.EXPENSES);
+  };
+
+  const handleClickResult = (e: React.MouseEvent) => {
+    setActiveTab(ETabs.RESULT);
+  };
+
   return (
     <div className="section">
       <div className="container">
-        <div className="tabs is-toggle  is-fullwidth">
+        <div className="tabs is-toggle is-fullwidth">
           <ul className={"is-block-mobile is-flex-desktop"}>
-            <li className="is-active is-marginless">
+            <li
+              className={`is-marginless ${activeTab === ETabs.FRIENDS &&
+                "is-active"}`}
+              onClick={handleClickFriends}
+            >
               <a>
-                <span className="icon is-small">
-                  <i className="fas fa-users" />
-                </span>
+                <IconContainer>
+                  <FontAwesomeIcon icon={faUser} />
+                </IconContainer>
                 <span>Add friends</span>
               </a>
             </li>
-            <li className={"is-marginless"}>
+            <li
+              className={`is-marginless ${activeTab === ETabs.EXPENSES &&
+                "is-active"}`}
+              onClick={handleClickExpenses}
+            >
               <a>
-                <span className="icon is-small">
-                  <i className="fas fa-dollar-sign" />
-                </span>
+                <IconContainer>
+                  <FontAwesomeIcon icon={faDollarSign} />
+                </IconContainer>
                 <span>Add expenses</span>
               </a>
             </li>
-            <li className={"is-marginless"}>
+            <li
+              className={`is-marginless ${activeTab === ETabs.RESULT &&
+                "is-active"}`}
+              onClick={handleClickResult}
+            >
               <a>
-                <span className="icon is-small">
-                  <i className="fas fa-chart-bar" />
-                </span>
+                <IconContainer>
+                  <FontAwesomeIcon icon={faChartBar} />
+                </IconContainer>
                 <span>See results</span>
               </a>
             </li>
