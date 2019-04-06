dd31944790ea8e7f1e697492f1d338633845d11b
Basti Buck <mail@bastibuck.de>
2019-04-06 12:09:13 +0200

    update existing friends on adding friends

--- src/store/reducer/friendReducer.ts
+++ src/store/reducer/friendReducer.ts
@@ -4,14 +4,34 @@ export const addFriendReducer = (
   state: typeof initialState,
   newFriend: IFriend,
 ): IRootStore => {
+  // clone friendsById
+  const newFriendsById = { ...state.friendsById };
+  const newFriendOwesObj = newFriend.owes;
+
+  // loop over all friends to add new friend to owes object
+  state.friends.forEach(friendId => {
+    // build old friends
+    newFriendsById[friendId] = {
+      ...newFriendsById[friendId],
+      owes: {
+        ...newFriendsById[friendId].owes,
+        [state.ids.nextFriendId]: 0,
+      },
+    };
+
+    // build new owes object for new friend
+    newFriendOwesObj[friendId] = 0;
+  });
+
   return {
     ...state,
     friends: [...state.friends, state.ids.nextFriendId],
     friendsById: {
-      ...state.friendsById,
+      ...newFriendsById,
       [state.ids.nextFriendId]: {
         ...newFriend,
         id: state.ids.nextFriendId,
+        owes: { ...newFriendOwesObj },
       },
     },
     ids: { ...state.ids, nextFriendId: state.ids.nextFriendId + 1 },
