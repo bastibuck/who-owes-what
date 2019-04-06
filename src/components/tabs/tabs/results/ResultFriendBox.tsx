94169398670edb695d0969d19a53d960f3b6fe25
Basti Buck <mail@bastibuck.de>
2019-04-06 11:04:04 +0200

    display correct total amount and evenState color

--- src/components/tabs/tabs/results/ResultFriendBox.tsx
+++ src/components/tabs/tabs/results/ResultFriendBox.tsx
@@ -16,29 +16,49 @@ enum EEvenState {
 
 const ResultFriendBox = ({ friendId }: IProps) => {
   // @ts-ignore
-  const [stateValue, dispatch]: [IRootStore, any] = useStateValue();
+  const [stateValue]: [IRootStore] = useStateValue();
+
+  const getTotal = () => {
+    let sum = 0;
+    Object.keys(friend.owes).map(owesFriendId => {
+      const owerId = parseInt(owesFriendId, 10);
+
+      sum += friend.owes[owerId];
+    });
+
+    return sum;
+  };
 
   // friend object
   const friend = stateValue.friendsById[friendId];
 
   // build strings
+  const total = getTotal();
+
   let cssColorClass = "";
-  let total = 0;
-  const rnd = Math.round(Math.random() * 10) % 3;
-  const evenState =
-    rnd === 0 ? EEvenState.EVEN : rnd === 1 ? EEvenState.OWES : EEvenState.GETS;
+  let evenState = EEvenState.EVEN;
+
+  if (total === 0) {
+    evenState = EEvenState.EVEN;
+  }
+
+  if (total > 0) {
+    evenState = EEvenState.OWES;
+  }
+
+  if (total < 0) {
+    evenState = EEvenState.GETS;
+  }
+
   switch (evenState) {
     case EEvenState.EVEN:
       cssColorClass = "has-background-info";
-      total = 0;
       break;
     case EEvenState.OWES:
       cssColorClass = "has-background-danger";
-      total = -23.45;
       break;
     case EEvenState.GETS:
       cssColorClass = "has-background-success";
-      total = 15.76;
       break;
 
     default:
@@ -46,52 +66,50 @@ const ResultFriendBox = ({ friendId }: IProps) => {
   }
 
   return (
-    <>
-      <div className={"column is-half"}>
-        <div className={`box has-text-white ${cssColorClass}`}>
-          <article className="media">
-            <div className="media-left">
-              <FontAwesomeIcon icon={faUser} />
-            </div>
-            <div className="media-content">
-              <div className="content">
-                <p>
-                  <strong className={`has-text-white ${cssColorClass}`}>
-                    {friend.name}
-                  </strong>
-                </p>
-                <div>
-                  <ul>
-                    {Object.keys(friend.owes).map(owesFriendId => {
-                      const owerId = parseInt(owesFriendId, 10);
+    <div className={"column is-half"}>
+      <div className={`box has-text-white ${cssColorClass}`}>
+        <article className="media">
+          <div className="media-left">
+            <FontAwesomeIcon icon={faUser} />
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong className={`has-text-white ${cssColorClass}`}>
+                  {friend.name}
+                </strong>
+              </p>
+              <div>
+                <ul>
+                  {Object.keys(friend.owes).map(owesFriendId => {
+                    const owerId = parseInt(owesFriendId, 10);
 
-                      if (friend.owes[owerId] > 0) {
-                        return (
-                          <li key={`owes-to-${owerId}`}>
-                            owes {friend.owes[owerId]} to{" "}
-                            {stateValue.friendsById[owerId].name}
-                          </li>
-                        );
-                      } else {
-                        return (
-                          <li key={`owes-to-${owerId}`}>
-                            gets {friend.owes[owerId] * -1} from{" "}
-                            {stateValue.friendsById[owerId].name}
-                          </li>
-                        );
-                      }
-                    })}
-                  </ul>
-                </div>
+                    if (friend.owes[owerId] > 0) {
+                      return (
+                        <li key={`owes-to-${owerId}`}>
+                          owes {friend.owes[owerId]} to{" "}
+                          {stateValue.friendsById[owerId].name}
+                        </li>
+                      );
+                    } else {
+                      return (
+                        <li key={`owes-to-${owerId}`}>
+                          gets {friend.owes[owerId] * -1} from{" "}
+                          {stateValue.friendsById[owerId].name}
+                        </li>
+                      );
+                    }
+                  })}
+                </ul>
               </div>
             </div>
-            <div className="media-right">
-              <sup>Total: {total}</sup>
-            </div>
-          </article>
-        </div>
+          </div>
+          <div className="media-right">
+            <sup>Total: {total}</sup>
+          </div>
+        </article>
       </div>
-    </>
+    </div>
   );
 };
 
