29494476f1eb168510b91ef70f4335b6c5b0e85c
Basti Buck <mail@bastibuck.de>
2019-04-18 10:04:02 +0200

    use ButtonDelete in FriendBox as well

--- src/components/common/ButtonDelete.tsx
+++ src/components/common/ButtonDelete.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
 interface IProps {
   readonly title: string;
-  readonly action: () => void;
+  readonly action: (e: React.MouseEvent) => void;
 }
 
+const StyledDelete = styled.button`
+  cursor: pointer;
+`;
+
 const ButtonDelete: React.FC<IProps> = ({ action, title }) => (
-  <button className="delete" role={"button"} onClick={action} title={title} />
+  <StyledDelete
+    className="delete"
+    role={"button"}
+    onClick={action}
+    title={title}
+  />
 );
 
 export default ButtonDelete;
