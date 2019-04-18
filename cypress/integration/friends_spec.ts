005d7785143ff467c495c397f86c4852551f8b2f
Basti Buck <mail@bastibuck.de>
2019-04-18 08:51:50 +0200

    correctly setup cypress

---
+++ cypress/integration/friends_spec.ts
@@ -0,0 +1,28 @@
+describe("Friends tab", function() {
+  it("should add multiple friends correctly", function() {
+    cy.visit("http://localhost:3000");
+    // friend tab active
+
+    // add button disabled
+
+    // other tabs disabled
+
+    // enter first friend name
+
+    // button becomes enabled
+
+    // added friend to friend list
+
+    // expenses tab enabled
+
+    // add another two friends (enter and button)
+
+    // three friends in friendlist
+
+    expect(false).toBeTruthy;
+  });
+
+  it("should delete friends correctly", () => {
+    expect(false).toBeTruthy;
+  });
+});
